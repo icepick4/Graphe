64f3e2a8b140391c617cb3c9d39893a968a0ee34
icepick4 <remi.jara@laposte.net>
2022-02-14 13:33:12 +0100

    add class graphe & graphe app

---
+++ src/graphe/Graphe.java
@@ -0,0 +1,212 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphe;
+
+/**
+ *
+ * @author Remi
+ */
+public class Graphe {
+    private int sommets;
+    private int arretes;
+    private int[][] matrice = new int [sommets][sommets];
+    
+    Graphe(int [][] matrice){
+        this.matrice = matrice;
+        this.sommets = matrice.length;
+        this.arretes = 0;
+        for(int[] ligne : this.matrice){
+            for(int colonne : ligne){
+                this.arretes+=colonne;
+            }
+        } 
+    }
+    
+    public int ordre(){
+        return this.sommets;
+    }
+    
+    public int taille(){
+        return this.arretes;
+    }
+    
+    public String type(){
+        int p = this.pGraphe();
+        if (this.estSimple()){
+            return "Ce graphe est simple";
+        }
+        else if(this.estElementaire()){
+            return "Ce graphe est un "+p+"-graphe élémentaire";
+        }
+        return "Ce graphe est un "+p+"graphe";
+    }
+    
+    public int pGraphe(){
+        int max = 0;
+        for (int[] ligne : this.matrice) {
+            for (int j = 0; j < this.matrice.length; j++) {
+                if (ligne[j] > max) {
+                    max = ligne[j];
+                }
+            }
+        }
+        return max;
+    }
+    
+    public boolean estElementaire(){
+        for(int i = 0; i < this.matrice.length; i++){
+            for(int j = 0; j < this.matrice.length; j++){
+                if(i == j && matrice[i][j] == 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean estSimple(){
+        return this.pGraphe() == 1 && this.estElementaire();
+    }
+    /**
+     * 
+     * @param sommet sommet auquel on récupère le ou les degrés (si orienté et non simple)
+     * @return un tableau de int correspondant au 
+     * degre du sommet, respectivement le degré simple, le demi-degré extérieur puis demi-degré intérieur
+     * (si orienté et non simple)
+     */
+    public int[] degre(int sommet){
+        int checkOriente;
+        if (this.estSymetrique()){
+            checkOriente = 1;
+        } 
+        else{
+            checkOriente = 3;
+        }
+        int[] degres = new int[checkOriente];
+        for(int i = 0; i<this.matrice.length; i++){
+            if (this.matrice[sommet][i] != 0){
+                degres[0]+=this.matrice[sommet][i];
+                if(degres.length > 1){
+                    degres[1]+=this.matrice[sommet][i];
+                }
+                
+            }
+            if (this.matrice[i][sommet] != 0){
+                if(degres.length > 1){
+                    degres[0]+=this.matrice[i][sommet];
+                    degres[2]+=this.matrice[i][sommet];
+                }
+            }
+        }
+        return degres;
+    }
+    
+    public int sommeDegre(){
+        return this.arretes*2;
+    }
+    /**
+     * 
+     * @param sommet sommet auquel on récupère les successeurs
+     * @return un tableau de int correspondant aux sommets successeurs de sommet
+     */
+    public int[] suivants(int sommet){
+        int checkOriente;
+        if (this.estSymetrique()){
+            checkOriente = 0;
+        } 
+        else{
+            checkOriente = 1;
+        }
+        int[] suivants = new int[this.degre(sommet)[checkOriente]];
+        int ctr = 0;
+        for(int i = 0; i < this.matrice.length; i++){
+            if(matrice[sommet][i] != 0){
+                suivants[ctr] = i;
+                ctr+=1;
+            }
+        }
+        return suivants;
+    }
+    /**
+     * 
+     * @param sommet sommet auquel on récupère les predecesseurs
+     * @return un tableau de int correspondant aux sommets predecesseurs de sommet
+     */
+    public int[] precedents(int sommet){
+        int checkOriente;
+        if (this.estSymetrique()){
+            checkOriente = 0;
+        } 
+        else{
+            checkOriente = 2;
+        }
+        int[] precedents = new int[this.degre(sommet)[checkOriente]];
+        int ctr = 0;
+        for(int i = 0; i < this.matrice.length; i++){
+            if(matrice[i][sommet] != 0){
+                precedents[ctr] = i;
+                ctr+=1;
+            }
+        }
+        return precedents;
+    }
+    /**
+     * 
+     * @param sommet1 
+     * @param sommet2 
+     * @return true si sommet1 est un predecesseur de sommet2, sinon false
+     */
+    public boolean verifPredecesseur(int sommet1, int sommet2){
+        int[] precedents = this.precedents(sommet2);
+        for(int i = 0; i < precedents.length; i++){
+            if(precedents[i] == sommet1){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * 
+     * @param sommet1 
+     * @param sommet2 
+     * @return true si sommet1 est un successeur de sommet2, sinon false
+     */
+    public boolean verifSuccesseur(int sommet1, int sommet2){
+        int[] suivants = this.suivants(sommet2);
+        for(int i = 0; i < suivants.length; i++){
+            if(suivants[i] == sommet1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean estSymetrique(){
+        for(int i = 0; i < this.matrice.length; i++){
+            for(int j = 0 ; j < this.matrice.length; j++){
+                if (this.matrice[i][j] != this.matrice[j][i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean estComplet(){
+        for(int i = 0; i < this.matrice.length; i++){
+            for(int j = 0; j < this.matrice.length; j++){
+                if(i != j && this.matrice[i][j] == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    } 
+    
+    public String toString(){
+        return "Nombre de sommets : "+this.ordre()+"\nNombre d'arc(s)/arrête(s) : "+this.taille()+
+                "\nSomme des degrés : "+this.sommeDegre()+"\nType du graphe : "+this.type();
+    }
+}
---
+++ src/graphe/GrapheApp.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package graphe;
+
+/**
+ *
+ * @author Remi
+ */
+public class GrapheApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [][] matrice = {
+                           {0,1,1},
+                           {1,0,1},
+                           {1,4,0}
+                          };
+        Graphe graphe1 = new Graphe(matrice);
+        System.out.println(graphe1.type());
+        afficherListe(graphe1.degre(2));
+        afficherListe(graphe1.suivants(0));
+        afficherListe(graphe1.precedents(0));
+        afficherListeDegres(graphe1);
+        System.out.println(graphe1.estComplet());
+        System.out.println(graphe1);    
+    }
+    public static void afficherListe(int[] tab){
+        System.out.print("[");
+        for(int i = 0; i < tab.length; i++){
+            System.out.print(tab[i]);
+            if (i + 1 != tab.length){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+    public static void afficherListeDegres(Graphe graphe){
+        System.out.print("              Sommet : ");
+        for(int i = 0; i < graphe.ordre(); i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for(int i = 0; i < 3; i++){
+            switch (i) {
+                case 0 -> {
+                    System.out.print("               Degré : ");
+                    for(int j = 0; j < graphe.ordre(); j++){
+                        System.out.print(graphe.degre(j)[0]+" ");
+                    }
+                    System.out.println();
+                }
+                case 1 -> {
+                    System.out.print("Demi-degré extérieur : ");
+                    for(int j = 0; j < graphe.ordre(); j++){
+                        System.out.print(graphe.degre(j)[1]+" ");
+                    }
+                    System.out.println();
+                }
+                default -> {
+                        System.out.print("Demi-degré intérieur : ");
+                        for(int j = 0; j < graphe.ordre(); j++){
+                                System.out.print(graphe.degre(j)[2]+" ");
+                                }
+                        System.out.println();
+                }
+            }
+            
+        }
+    }
+    
+    
+}
