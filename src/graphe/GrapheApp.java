726385d55f44f6ce5db5418c48962c5218290d19
icepick4 <remi.jara@laposte.net>
2022-02-18 10:14:51 +0100

    nettoyage

--- src/graphe/GrapheApp.java
+++ src/graphe/GrapheApp.java
@@ -96,6 +96,7 @@ public class GrapheApp {
         
         
     }
+    
     public static void afficherListe(int[] tab){
         System.out.print("[");
         for(int i = 0; i < tab.length; i++){
@@ -106,36 +107,5 @@ public class GrapheApp {
         }
         System.out.println("]");
     }
-    public static void afficherListeDegres(Graphe graphe){
-        System.out.print("              Sommet : ");
-        for(int i = 0; i < graphe.ordre(); i++){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        for(int i = 0; i < 3; i++){
-            switch (i) {
-                case 0 -> {
-                    System.out.print("               Degré : ");
-                    for(int j = 0; j < graphe.ordre(); j++){
-                        System.out.print(graphe.degre(j)[0]+" ");
-                    }
-                    System.out.println();
-                }
-                case 1 -> {
-                    System.out.print("Demi-degré extérieur : ");
-                    for(int j = 0; j < graphe.ordre(); j++){
-                        System.out.print(graphe.degre(j)[1]+" ");
-                    }
-                    System.out.println();
-                }
-                default -> {
-                        System.out.print("Demi-degré intérieur : ");
-                        for(int j = 0; j < graphe.ordre(); j++){
-                                System.out.print(graphe.degre(j)[2]+" ");
-                                }
-                        System.out.println();
-                }
-            } 
-        }
-    }
+    
 }
